14cac8e9444a3648b81d65d44861760fb8d7d44c
nabin6246 <nabin62246@gmail.com>
2022-09-21 20:14:46 +0545

    Add readme for general introduction

--- tests/Main.hs
+++ tests/Main.hs
@@ -1,7 +1,18 @@
-
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE TypeApplications #-}
 
 module Main
 where
+
 
 
 main = do
