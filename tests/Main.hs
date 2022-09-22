8778370b656cbcb30c85e4ca0347ed03ed886c1d
nabin6246 <nabin62246@gmail.com>
2022-09-22 20:54:26 +0545

    Add color coding and both tests running endpoint

--- tests/Main.hs
+++ tests/Main.hs
@@ -13,7 +13,18 @@
 module Main
 where
 
+import qualified EcdsaSecp256k1Tests as EcdsaSecp256k1Tests
+import qualified SchnorrSecp256k1Tests as SchnorrSecp256k1Tests
+import GHC.IO.Handle.FD (stdout)
+import System.IO (BufferMode (NoBuffering), hSetBuffering)
 
-
+main :: IO ()
 main = do
-    putStrLn "ok"
+    hSetBuffering stdout NoBuffering
+    putStrLn "\ESC[32m"
+    EcdsaSecp256k1Tests.tests
+    putStrLn "\n----------------------------------------------------------------------------------------"
+    SchnorrSecp256k1Tests.tests
+
+
+
