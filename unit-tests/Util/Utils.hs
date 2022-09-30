608e68ddda6c572dd631d1c3ff31be998a5e3dd4
nabin6246 <nabin62246@gmail.com>
2022-09-30 09:16:13 +0545

    Add Base 16 encode decode to utils

--- unit-tests/Util/Utils.hs
+++ unit-tests/Util/Utils.hs
@@ -9,8 +9,23 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 
 module Util.Utils
 where
 
+import Data.ByteString ( ByteString )
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Base16 as BS16
+import Cardano.Binary (ToCBOR, serialize')
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
+
+-- Convert raw bytes to base16
+toHex :: ToCBOR a => a -> Int -> String
+toHex a dropFront = T.unpack $ T.decodeUtf8 $ BS.drop dropFront $ BS16.encode $ serialize' a
+
+--Convert bas16 to raw bytes
+unHex :: ByteString -> Either String ByteString
+unHex = BS16.decode
